3234ac83d85d25ce96e60391f9a9268fb1c89679
Clint Valentine <valentine.clint@gmail.com>
2014-11-21 19:45:29 -0500

    Time converter for dumbass excel

--- common.py
+++ common.py
@@ -21,12 +21,27 @@ reload(csv_to_df)
 from __future__ import print_function
 __version__ = '21.11.2014'
 
+import os
 import sys
+import glob
+import datetime
 import pandas as pd
 
-def csv_to_df(filename=None):
+def builddata():
+    os.chdir("/data")
+    datafiles = []
+    for filename in glob.glob("*.csv"):
+        print(filename)
+
+def csv_to_df(filename=None, index_col=None):
     """Pass relative filename string and return pandas DataFrame"""
-    return pd.read_csv('data\\{0}'.format(filename), sep="\t", header=0)
+    return pd.read_csv('data\\{0}'.format(filename), sep="\t", header=0,
+                       index_col=index_col)
+
+def convert_time(time=None):
+    """ Converts Excel days since Dec. 31st 1899 to current time"""
+    return datetime.date(1899, 12, 30) + datetime.timedelta(days=time)
+
 
 
 if __name__ == '__main__':
