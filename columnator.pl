5f96b66453e2640be4c45282c512dfec9a4ec51d
Andres Breton <bretonics@users.noreply.github.com>
2014-11-22 00:16:05 -0500

    added user view/create file

--- columnator.pl
+++ columnator.pl
@@ -7,10 +7,10 @@ use feature qw(say);
 
 #####################
 #
-# 	Copyright © Andres Breton
-#	File: columnator.pl
+#   Copyright © Andres Breton
+#   File: columnator.pl
 #
-#   Extract desired column pairs for comparison/other IO
+#   Extract desired column pairs for comparison or other IO uses
 #
 #####################
 
@@ -20,15 +20,16 @@ use feature qw(say);
 
 my $inFile = $ARGV[0];
 my $outFile;
+my $outputFileName = "columnator_out.csv";  #Change desired output file
 
-my $column1 = 8; #Change to desired column pairs
-my $column2 = 18; #Remember to start count at 0
+my $column1 = 8;    #Change to desired column pairs
+my $column2 = 18;   #Remember to start count at 0
 
 my ($col1, $col2) = columnPairs($column1, $column2);
 my @col1 = @$col1;
 my @col2 = @$col2;
 
-my $col1Size = @col1; #Number of
+my $col1Size = @col1;   #Number of data points extracted
 my $col2Size = @col2;
 
 
@@ -43,8 +44,37 @@ if(! $inFile) {
 ##########################
 # USER INPUT
 
+say "\n$col1Size number of data points extracted";
 
+my $response = "none";
 
+until ($response eq "view" || $response eq "file") {
+    print "\nWould you like to view or create file with data? ";
+    my $response = lc <STDIN>;
+    chomp $response;
+    
+    if ($response eq "view") {
+        for (my $i = 0; $i < $col1Size; $i++) {
+            say "$col1[$i]    $col2[$i]";
+        }
+        exit;
+    }elsif ($response eq "file") {
+        $outFile = $outputFileName;
+        
+        unless(open(OUTFILE, ">", $outFile)){
+            die "Can not open output file $outFile for writing", "\n", $!;
+        }
+        
+        for (my $i = 0; $i < $col1Size; $i++) {
+            say OUTFILE "$col1[$i]\t$col2[$i]";
+        }
+        close OUTFILE;
+        say "File $outputFileName created successfully";
+        exit;
+    }else {
+        say "Please provide answer as \"view\" or \"file\"";
+    }
+}
 
 ##########################
 # SUB
